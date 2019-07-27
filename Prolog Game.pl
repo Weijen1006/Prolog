ab6ec5b617c1efc8c62ffd97a264d3b9f57fe2ad
Weijen1006 <41354172+Weijen1006@users.noreply.github.com>
2019-07-27 22:04:32 +0800

    minor update

--- Prolog Game.pl
+++ Prolog Game.pl
@@ -1,13 +1,22 @@
-start :- write('Starting of the story'),nl,town.
+start :- retractall(item(_,_)),retractall(weapon(_)),retractall(score(_)),retractall(hp(_)),
+	 assert(hp(fresh)),assert(score(0)),assert(weapon(nothing)),
+	 assert(item(potion,0)),
+	 assert(item(blowtorch,0)),
+	 assert(item(pepper,0)),
+	 assert(item(mt,no)),
+	 assert(item(co,no)),
+	 assert(item(corkscrew,no)),
+	 assert(enemy(no)),
+	 write('Starting of the story'),nl,town.
 
 town :- write('Please choose a place to go'),nl,
 	write('Your choice : '),read(X),location(X).
 
 location(up) :- write('You choose neutral faction, Good Luck'),nl,neutral.
 location(left) :- write('You choose ice faction, Good Luck'),nl,ice.
-location(right):- write('You choose fire faction, Good Luck'),nl,fire.
-location(down):- write('I see what you try to do there, but there is no going back'),nl,write('Your choice : '),read(X),location(X).
-location(_):- write('Invalid Input, Please try again'),nl,write('Your choice : '),read(X),location(X).
+location(right) :- write('You choose fire faction, Good Luck'),nl,fire.
+location(down) :- write('I see what you try to do there, but there is no going back'),nl,write('Your choice : '),read(X),location(X).
+location(_) :- write('Invalid Input, Please try again'),nl,write('Your choice : '),read(X),location(X).
 
 %Neutral faction
 neutral :- write('Stay tuned').
@@ -24,3 +33,16 @@ fire :- write('Stay tuned').
 
 %Battle
 
+help :- write('List of helpful command'),nl,
+	write('itemlist. - to check available item in your inventory'),nl,
+	write('potion. - to use tomato juice'),nl,
+	write('blowtorch. - only can use on enemy'),nl,
+	write('pepper. - only can use on enemy'),!.
+
+itemlist :- weapon(X), write('Weapon : '),write(X),nl,
+	    item(potion,Y), write('Tomato juice   x '),write(Y),nl,
+	    item(blowtorch,Z), write('Blowtorch   x '),write(Z),nl,
+	    item(pepper,A), write('Pepper   x '),write(A),nl,
+	    ((item(mt,yes), write('Meat Tenderizer'),nl);(item(mt,no),!)),
+	    ((item(co,yes), write('Can Opener'),nl);(item(co,no),!)),
+	    ((item(corkscrew,yes), write('Corkscrew'),nl);(item(corkscrew,no),!)),!.
