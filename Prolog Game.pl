d0076199ef999523cff31d5a8b9c2555eedd74c7
Weijen1006 <41354172+Weijen1006@users.noreply.github.com>
2019-07-28 17:11:32 +0800

    battle

--- Prolog Game.pl
+++ Prolog Game.pl
@@ -1,12 +1,11 @@
 start :- retractall(item(_,_)),retractall(weapon(_)),retractall(score(_)),retractall(hp(_)),
-	 assert(hp(fresh)),assert(score(0)),assert(weapon(nothing)),
-	 assert(item(potion,0)),
+	 assert(hp(fresh)),assert(score(0)),assert(weapon(woodenspatula)),
+	 assert(item(potion,3)),
 	 assert(item(blowtorch,0)),
 	 assert(item(pepper,0)),
 	 assert(item(mt,no)),
 	 assert(item(co,no)),
 	 assert(item(corkscrew,no)),
-	 assert(enemy(no)),
 	 write('Starting of the story'),nl,town.
 
 town :- write('          | N |          '),nl,
@@ -40,11 +39,34 @@ ice :- write('Stay tuned').
 fire :- write('Stay tuned').
 
 %HP
+deducthp :- hp(fresh), retractall(hp(_)), assert(hp(spoil)).
+deducthp :- hp(spoil), retractall(hp(_)), assert(hp(rotten)).
+deducthp :- hp(rotten), retractall(hp(_)), assert(hp(die)).
+deducthp :- hp(die),!.
+
+battlestatus :- hp(X), write('Player Hp : '),write(X),nl,
+		enemy(Name,Hp), write(Name), write(' Hp : '),write(Hp),nl.
+
 
 %Monster
+enemyattack(meatball) :- write('Meatball attack!   Player hp - 1'), nl,nl, deducthp. 
+
+%Weapon
+weaponattack(woodenspatula) :- random(1,101,X), woodenspatula(X).
+woodenspatula(X) :- X =< 35,nl, write('Your attack missed!'),nl.
+woodenspatula(X) :- X > 35,nl, write('Its a direct hit! 1 damage dealt'),nl, enemy(Name,Hp), Newhp is Hp-1, retractall(enemy(_,_)), assert(enemy(Name,Newhp)).
 
 %Battle
+battle :- hp(X), enemy(_,Y), X \= die, Y > 0, write('What will you do next? :'), read(Z), action(Z), battle; result.
 
+action(attack) :- weapon(X), weaponattack(X),nl,battlestatus,nl, enemy(Name,Hp), Hp > 0, enemyattack(Name),battlestatus,nl.
+action(attack) :- enemy(_,Hp), Hp =< 0,!.
+action(_) :- write('Invalid action, please try again'), nl, write('What will you do next? : '),read(X) ,action(X).
+
+result :- hp(die), write('You die'),nl.
+result :- enemy(Name,Hp), Hp =< 0, write('You defeated '), write(Name), write('! Good Job!'),nl ,retractall(enemy(_,_)).
+
+%Utility
 help :- write('List of helpful command'),nl,
 	write('itemlist. - to check available item in your inventory'),nl,
 	write('potion. - to use tomato juice'),nl,
@@ -58,3 +80,5 @@ itemlist :- weapon(X), write('Weapon : '),write(X),nl,
 	    ((item(mt,yes), write('Meat Tenderizer'),nl);(item(mt,no),!)),
 	    ((item(co,yes), write('Can Opener'),nl);(item(co,no),!)),
 	    ((item(corkscrew,yes), write('Corkscrew'),nl);(item(corkscrew,no),!)),!.
+
+testevent :- write('A wild meatball appears !'),assert(enemy(meatball,2)),nl, battle.
